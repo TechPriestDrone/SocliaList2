e0e7b5718e099b04241dcf8f68d1126952930108
Andrey <fordeving@icloud.com>
2023-10-15 17:40:58 +0300

    Initial Commit

---
+++ SocliaList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SocliaList
+//
+//  Created by Andrey Safronov on 15/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SocliaList/SocliaListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocliaListApp.swift
+//  SocliaList
+//
+//  Created by Andrey Safronov on 15/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SocliaListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
